058c452f1ad36aced20c861aad458004ce4d736e
Vojtěch Kubala <vojtechkubala@outlook.com>
2025-07-02 21:20:21 +0200

    Added node: true config to the .eslintrc.js file

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from 'globals'
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+  },
+]
