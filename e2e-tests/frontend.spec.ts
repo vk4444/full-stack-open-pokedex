7425206c3af05dbf814d11491f1200c79bef5c78
Vojtěch Kubala <vojtechkubala@outlook.com>
2025-07-03 09:44:54 +0200

    added test for navigation from front page

--- e2e-tests/frontend.spec.ts
+++ e2e-tests/frontend.spec.ts
@@ -2,8 +2,14 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('/')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  
+  test('A specific pokémon can be opened from the frontpage', async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
